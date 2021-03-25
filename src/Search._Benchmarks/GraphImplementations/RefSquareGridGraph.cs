86bf764efa9324750eeebb349122bc26621c42ff
Simon Condon <simoncondon@hotmail.com>
2021-03-25 18:27:39 +0000

    Added benchmarks project.

---
+++ src/Search._Benchmarks/GraphImplementations/RefSquareGridGraph.cs
@@ -0,0 +1,59 @@
+﻿using SCGraphTheory.AdjacencyList;
+
+namespace Search.Benchmarks.GraphImplementations
+{
+    public class RefSquareGridGraph
+    {
+        public static Graph<Node, Edge> Create((int X, int Y) size, out Node originNode)
+        {
+            var graph = new Graph<Node, Edge>();
+            var nodeIndex = new Node[size.X, size.Y];
+
+            for (int x = 0; x < size.X; x++)
+            {
+                for (int y = 0; y < size.Y; y++)
+                {
+                    graph.Add(nodeIndex[x, y] = new Node(x, y));
+                }
+            }
+
+            for (int x = 0; x < size.X; x++)
+            {
+                for (int y = 0; y < size.Y; y++)
+                {
+                    for (int dx = -1; dx <= 1; dx++)
+                    {
+                        if (nodeIndex.GetLowerBound(0) <= x + dx && nodeIndex.GetUpperBound(0) >= x + dx)
+                        {
+                            for (int dy = -1; dy <= 1; dy++)
+                            {
+                                if ((dx != 0 || dy != 0) && nodeIndex.GetLowerBound(1) <= y + dy && nodeIndex.GetUpperBound(1) >= y + dy)
+                                {
+                                    graph.Add(new Edge(nodeIndex[x, y], nodeIndex[x + dx, y + dy]));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            originNode = nodeIndex[0, 0];
+            return graph;
+        }
+
+        public class Edge : EdgeBase<Node, Edge>
+        {
+            public Edge(Node from, Node to)
+                : base(from, to)
+            {
+            }
+        }
+
+        public class Node : NodeBase<Node, Edge>
+        {
+            public Node(int x, int y) => Coordinates = (x, y);
+
+            public (int X, int Y) Coordinates { get; }
+        }
+    }
+}
---
+++ src/Search._Benchmarks/Program.cs
@@ -0,0 +1,93 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using SCGraphTheory.AdjacencyList;
+using SCGraphTheory.Search.Classic;
+using Search.Benchmarks.GraphImplementations;
+using System;
+
+namespace Search.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class SearchBenchmarks
+    {
+        private const int SIZE = 20;
+
+        private readonly ValSquareGridGraph<bool> valGraph;
+        private readonly Graph<RefSquareGridGraph.Node, RefSquareGridGraph.Edge> refGraph;
+        private readonly RefSquareGridGraph.Node originNode;
+
+        public SearchBenchmarks()
+        {
+            valGraph = new ValSquareGridGraph<bool>((SIZE, SIZE));
+            refGraph = RefSquareGridGraph.Create((SIZE, SIZE), out originNode);
+        }
+
+        [Benchmark]
+        public ValSquareGridGraph<bool> MakeValGraph() => new ValSquareGridGraph<bool>((SIZE, SIZE));
+
+        [Benchmark]
+        public Graph<RefSquareGridGraph.Node, RefSquareGridGraph.Edge> MakeRefGraph() => RefSquareGridGraph.Create((SIZE, SIZE), out _);
+
+        [Benchmark]
+        public void ValBFS() => new BreadthFirstSearch<ValSquareGridGraph<bool>.Node, ValSquareGridGraph<bool>.Edge>(
+            valGraph[0, 0],
+            n => n.Coordinates == (SIZE - 1, SIZE - 1)).Complete();
+
+        [Benchmark]
+        public void ValDFS() => new DepthFirstSearch<ValSquareGridGraph<bool>.Node, ValSquareGridGraph<bool>.Edge>(
+            valGraph[0, 0],
+            n => n.Coordinates == (SIZE - 1, SIZE - 1)).Complete();
+
+        [Benchmark]
+        public void ValDijkstra() => new DijkstraSearch<ValSquareGridGraph<bool>.Node, ValSquareGridGraph<bool>.Edge>(
+            valGraph[0, 0],
+            n => n.Coordinates == (SIZE - 1, SIZE - 1),
+            e => EuclideanDistance(e.To.Coordinates, e.From.Coordinates)).Complete(); // NB: unfair test - largely constant costs means Dijkstra's pretty much same as BFS with more work..
+
+        [Benchmark]
+        public void ValAStar() => new AStarSearch<ValSquareGridGraph<bool>.Node, ValSquareGridGraph<bool>.Edge>(
+            valGraph[0, 0],
+            n => n.Coordinates == (SIZE - 1, SIZE - 1),
+            e => EuclideanDistance(e.To.Coordinates, e.From.Coordinates),
+            n => EuclideanDistance((SIZE - 1, SIZE - 1), n.Coordinates)).Complete();
+
+        [Benchmark]
+        public void RefBFS() => new BreadthFirstSearch<RefSquareGridGraph.Node, RefSquareGridGraph.Edge>(
+            originNode,
+            n => n.Coordinates == (SIZE - 1, SIZE - 1)).Complete();
+
+        [Benchmark]
+        public void RefDFS() => new DepthFirstSearch<RefSquareGridGraph.Node, RefSquareGridGraph.Edge>(
+            originNode,
+            n => n.Coordinates == (SIZE - 1, SIZE - 1)).Complete();
+
+        [Benchmark]
+        public void RefDijkstra() => new DijkstraSearch<RefSquareGridGraph.Node, RefSquareGridGraph.Edge>(
+            originNode,
+            n => n.Coordinates == (SIZE - 1, SIZE - 1),
+            e => EuclideanDistance(e.To.Coordinates, e.From.Coordinates)).Complete(); // NB: unfair test - largely constant costs means Dijkstra's pretty much same as BFS with more work..
+
+        [Benchmark]
+        public void RefAStar()
+        {
+            new AStarSearch<RefSquareGridGraph.Node, RefSquareGridGraph.Edge>(
+                originNode,
+                n => n.Coordinates == (SIZE - 1, SIZE - 1),
+                e => EuclideanDistance(e.To.Coordinates, e.From.Coordinates),
+                n => EuclideanDistance((SIZE - 1, SIZE - 1), n.Coordinates)).Complete();
+        }
+
+        private static float EuclideanDistance((int x, int y) a, (int x, int y) b)
+        {
+            return (float)Math.Sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y));
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<SearchBenchmarks>();
+        }
+    }
+}
